9c918bf3a03774f93f5960cf46ca33c203892bed
sarfraz6548 <157268159+sarfraz6548@users.noreply.github.com>
2024-01-21 11:16:55 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,10 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD0GZqe3HY2h4yj00m4DxynFf77GI2aS8Q",
+  authDomain: "gsdr7fvg.firebaseapp.com",
+  databaseURL: "https://gsdr7fvg-default-rtdb.firebaseio.com",
+  projectId: "gsdr7fvg",
+  storageBucket: "gsdr7fvg.appspot.com",
+  messagingSenderId: "418977429834",
+  appId: "1:418977429834:web:4f29c2376c001d5fa0a9b3"
+};
